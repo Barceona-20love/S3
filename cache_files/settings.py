95fcced1f098ba5e8e3bc691fa0850a7e4cc57ec
User <loading.kis@gmail.com>
2022-11-21 15:24:26 +0300

    install commit

---
+++ cache_files/settings.py
@@ -0,0 +1,17 @@
+import os
+
+from project.project.settings import BASE_DIR
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': os.path.join(BASE_DIR, 'cache_files'),
+        'TIMEOUT': 60,
+    }
+}
+
+MIDDLEWARE = [
+    'django.middleware.cache.UpdateCacheMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
+    ]
